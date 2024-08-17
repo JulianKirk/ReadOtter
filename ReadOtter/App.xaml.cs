e6a801a011ebb44aff304e10f801f1886b19c419
JulianKirk <progamerguy005@gmail.com>
2024-08-18 02:17:03 +1000

    Add project files.

---
+++ ReadOtter/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ReadOtter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
