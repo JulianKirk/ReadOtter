3581ef9acc7b118b0dbdcb3f8bd07356abbb21fa
JulianKirk <progamerguy005@gmail.com>
2024-08-21 20:59:38 +1000

    Epub d;fkjljs;ks;ldss;gdsfs;dv

--- ReadOtter.Shared/Data/Services/EpubService.cs
+++ ReadOtter.Shared/Data/Services/EpubService.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using ReadOtter.Shared.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VersOne.Epub;
+using VersOne.Epub.Options;
 
 namespace ReadOtter.Shared.Data.Services
 {
@@ -10,6 +13,35 @@ namespace ReadOtter.Shared.Data.Services
     {
         public EpubService(UnitOfWork unitOfWork) :base(unitOfWork) 
         {
+        }
+
+        readonly static EpubReaderOptions defaultReaderOptions = new EpubReaderOptions();
+
+        public EpubBookRef GetEpubBookRef(int id, EpubReaderOptions? readerOptions = null)
+        {
+            var book = _unitOfWork.BookRepository.GetBookById(id);
+
+            var options = readerOptions ?? defaultReaderOptions;
+
+            return EpubReader.OpenBook(book.FilePath, options);
+        }
+
+        public EpubBook GetEpubBook(int id, EpubReaderOptions? readerOptions = null)
+        {
+            var book = _unitOfWork.BookRepository.GetBookById(id);
+
+            var options = readerOptions ?? defaultReaderOptions;
+
+            return EpubReader.ReadBook(book.FilePath, options);
+        }
+
+        public EpubLocalTextContentFile GetCurrentChapterContent(int id)
+        {
+            var epubBook = GetEpubBook(id);
+
+            var book = _unitOfWork.BookRepository.GetBookById(id);
+
+            return epubBook.Navigation[book.CurrentChapter].HtmlContentFile;
         }
     }
 }
