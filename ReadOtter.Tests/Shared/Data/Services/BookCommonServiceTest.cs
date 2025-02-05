6d4c2d92483fa5c1515df7b4e0ab5c7f54e90b17
JulianKirk <juliankirk2005@gmail.com>
2025-02-05 21:51:22 +1100

    Wrote some tests

--- ReadOtter.Tests/Shared/Data/Services/BookCommonServiceTest.cs
+++ ReadOtter.Tests/Shared/Data/Services/BookCommonServiceTest.cs
@@ -1,33 +1,76 @@
 ﻿using Moq;
 using ReadOtter.Shared.Data;
+using ReadOtter.Shared.Data.Models;
 using ReadOtter.Shared.Data.Repositories;
+using ReadOtter.Shared.Data.Services;
 
 namespace ReadOtter.Tests.Shared.Data.Services
 {
 	public class BookCommonServiceTest
 	{
-		//[SetUp]
-		//public void SetUp()
-		//{
-		//	var mockBookRepository = new Mock<IBookRepository>();
-		//	mockUnitOfWork = new Mock<IUnitOfWork>();
-
-		//	mockUnitOfWork.Setup(u => u.BookRepository).Returns();
-		//}
 
 		[Test]
 		public void TestGetAllBooks()
 		{
-			Assert.Pass();
+			//Arrange
+			var books = new List<Book>
+			{
+				new Book {Id = 1},
+				new Book {Id = 2},
+				new Book {Id = 3},
+				new Book {Id = 4},
+				new Book {Id = 5}
+			};
+
+			var unitOfWork = MockUnitOfWork(books);
+			var commonService =  new BookCommonService(unitOfWork);
+
+			//Act
+			var bookList = commonService.GetAllBooks();
+
+			//Assert
+			Assert.That(bookList, Is.EqualTo(books));
 		}
 
 		[Test]
 		public void TestGetAllBookIds()
 		{
-			Assert.Pass();
+			//Arrange
+			var books = new List<Book>
+			{
+				new Book {Id = 1},
+				new Book {Id = 2},
+				new Book {Id = 3},
+				new Book {Id = 4},
+				new Book {Id = 5}
+			};
+
+			var unitOfWork = MockUnitOfWork(books);
+			var commonService = new BookCommonService(unitOfWork);
+
+			//Act
+			var bookList = commonService.GetAllBookIds();
+
+			//Assert
+			Assert.That(bookList, Is.EqualTo(new List<int> { 1, 2, 3, 4, 5}));
 		}
 
+		IUnitOfWork MockUnitOfWork(List<Book> books)
+		{
+			var mockBookRepository = new Mock<IBookRepository>();
+			mockBookRepository.Setup(r => r.GetAllBooks()).Returns(books);
+			mockBookRepository.Setup(r => r.RemoveBookById(It.IsAny<int>()))
+				.Callback<int>(id =>
+				{
+					var bookToRemove = books.FirstOrDefault(b => b.Id == id);
+					books.Remove(bookToRemove);
+				});
 
-		Mock<IUnitOfWork> mockUnitOfWork;
+			var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+			mockUnitOfWork.Setup(u => u.BookRepository).Returns(mockBookRepository.Object);
+
+			return mockUnitOfWork.Object;
+		}
 	}
 }
